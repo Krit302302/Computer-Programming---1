b5c33304043f5134705e097d004b5e0152709cd1
Krit302302 <kritpatel302302@gmail.com>
2025-08-24 15:52:00 +0530

    Add files via upload

---
+++ 1.Add two numbers.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+float a,b;
+printf("enter two number");
+scanf("%f %f",&a,&b);
+printf("sum is %f",a+b);
+}
+
---
+++ 2.Subtract two numbers.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+float a,b;
+printf("enter two number");
+scanf("%f %f",&a,&b);
+printf("answer is %f",a-b);
+}
